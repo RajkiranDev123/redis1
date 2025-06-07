880acb868cfca8aa8e4478e882753753297e81d3
raj <rajtech645@gmail.com>
2025-06-07 08:27:25 +0530

    redis done

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 
 const client=require("./client.js")
+const axios=require("axios")
+const express=require("express")
+
+const app=express()
+
+app.get("/",async (req,res)=>{
+   try {
+     const cachedValues=await client.get("todos")
+     if(cachedValues){
+         return res.json(JSON.parse(cachedValues))
+     }
+     const {data}=await axios.get("https://jsonplaceholder.typicode.com/todos")
+     console.log("first",typeof data)
+ 
+     await client.set("todos",JSON.stringify(data))
+     await client.expire("todos",10)
+ 
+     return res.json(data)
+   } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+   }
+})
+app.listen(5000)
